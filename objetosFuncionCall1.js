f0c2ff490c3003f3b6336fda141ece7eda49681f
Marchi14 <45884562+Marchi14@users.noreply.github.com>
2019-10-15 16:20:07 +0200

    s

--- objetosFuncionCall1.js
+++ objetosFuncionCall1.js
@@ -1,15 +1,15 @@
 var person = {
-  fullName: function() {
-    return this.firstName + " " + this.lastName;
-  }
+    fullName: function() {
+        return this.firstName + " " + this.lastName;
+    }
 }
 var person1 = {
-  firstName:"John",
-  lastName: "Doe"
+    firstName: "John",
+    lastName: "Doe"
 }
 var person2 = {
-  firstName:"Mary",
-  lastName: "Doe"
+    firstName: "Mary",
+    lastName: "Doe"
 }
-person.fullName.call(person1);  // Muestra "John Doe". Aquí el objeto que actua como this es person1
-person.fullName.call(person2);  // Muestra "Mary Doe". En este caso, quién actua como this es person2
+person.fullName.call(person1); // Muestra "John Doe". Aquí el objeto que actua como this es person1
+person.fullName.call(person2); // Muestra "Mary Doe". En este caso, quién actua como this es person2
